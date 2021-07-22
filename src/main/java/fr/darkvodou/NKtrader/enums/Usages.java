7b8296d301d2ba80ef54ec50235eb8dbe484811b
darkvodou <facebookger@live.fr>
2021-07-22 17:43:11 +0200

    fix pull request

--- src/main/java/fr/darkvodou/NKtrader/enums/Usages.java
+++ src/main/java/fr/darkvodou/NKtrader/enums/Usages.java
@@ -4,17 +4,11 @@ import static fr.darkvodou.NKtrader.enums.MsgUtils.*;
 
 public enum Usages
 {
-	PLUGIN_NAME("NKtrader"),
-
-
 	//#########################
 	//Usages for commands
 	//#########################
 
-	NKT_USAGE_ADDTRADER(PREFIX_USAGE + "/addtrader <entity|block> [-n <name>] [-w <world> <x> <y> <z> ]"),
-
-
-	;
+	NKT_USAGE_ADDTRADER(PREFIX_USAGE + "/addtrader <entity|block> [-n <name>] [-w <world> <x> <y> <z> ]");
 
 	private String name;
 
--- src/main/java/fr/darkvodou/NKtrader/managers/TraderManager.java
+++ src/main/java/fr/darkvodou/NKtrader/managers/TraderManager.java
@@ -10,21 +10,29 @@ import static fr.darkvodou.NKtrader.enums.Msgmanager.*;
 public class TraderManager
 {
 	private HashMap<String, Trader> traders = new HashMap<>();
+
 	public ConsoleCommandSender consoleCommandSender;
 
 	public TraderManager(ConsoleCommandSender consoleCommandSender)
-	{this.consoleCommandSender = consoleCommandSender;}
+	{
+		this.consoleCommandSender = consoleCommandSender;
+	}
 
-	public boolean hasTrader(String name){return traders.containsKey(name);}
+	public boolean hasTrader(String name)
+	{
+		return traders.containsKey(name);
+	}
+
 	public void addTrader(String name, Trader trader)
 	{
 		if(hasTrader(name))
 			consoleCommandSender.sendMessage("" + ERROR_TRADER_IS_CONTAINED);
 		else
 		{
-			traders.put(name,trader);
+			traders.put(name, trader);
 		}
 	}
+
 	public void removeTrader(String name)
 	{
 		if(!hasTrader(name))
@@ -36,8 +44,16 @@ public class TraderManager
 			traders.remove(name);
 		}
 	}
-	public Trader getTrader(String name){return traders.get(name);}
-	public HashMap<String, Trader> getTraders(){return this.traders;}
+
+	public Trader getTrader(String name)
+	{
+		return traders.get(name);
+	}
+
+	public HashMap<String, Trader> getTraders()
+	{
+		return this.traders;
+	}
 
 	//TODO Load Traders in bdd
 }
